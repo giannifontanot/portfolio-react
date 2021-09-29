0800c458a681e78eb4a7c551ead117b13216ea4e
giannifontanot <giannifontanot@gmail.com>
2021-09-29 18:29:56 -0500

    Open PDF file.

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -28,7 +28,7 @@ function NavTabs({pageToDraw, handlePageChange}) {
                             </td>
                             <td className={pageToDraw === 'Resume' ? 'nav-table-cell-active' : 'nav-table-cell'}>
                                 <a className="nav-item-right"
-                                   href={process.env.PUBLIC_URL + "/assets/pdf/resume.pdf"} download>resume
+                                   href={process.env.PUBLIC_URL + "/assets/pdf/resume.pdf"} >resume
                                 </a>
                             </td>
                             <td className={pageToDraw === 'Contact' ? 'nav-table-cell-active' : 'nav-table-cell'}>

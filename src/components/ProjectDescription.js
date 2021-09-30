1b39f451326c82f8238c7803dbdc1a8c771dfad8
giannifontanot <giannifontanot@gmail.com>
2021-09-29 22:05:21 -0500

    Centered picture.

--- src/components/ProjectDescription.js
+++ src/components/ProjectDescription.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ProjectDescription(props) {
     return (
         <div className="ProjectDescription">
-            <div><h1>{props.title}</h1></div>
+            <div><h1><strong>{props.title}</strong></h1></div>
             <div><h2>{props.subtitle}</h2></div><br />
             <div><p>{props.content}</p></div>
         </div>

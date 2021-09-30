b2ff902767d42bb8b388f0c022d2e2033dae8141
giannifontanot <giannifontanot@gmail.com>
2021-09-29 20:52:44 -0500

    Font size updated.

--- src/components/ProjectDescription.js
+++ src/components/ProjectDescription.js
@@ -4,7 +4,7 @@ export default function ProjectDescription(props) {
     return (
         <div className="ProjectDescription">
             <div><h1>{props.title}</h1></div>
-            <div><h4>{props.subtitle}</h4></div>
+            <div><h2>{props.subtitle}</h2></div><br />
             <div><p>{props.content}</p></div>
         </div>
 

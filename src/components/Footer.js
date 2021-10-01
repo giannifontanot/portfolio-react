2baf35d94aa299acb0b2800726939b76d315dc3c
giannifontanot <giannifontanot@gmail.com>
2021-10-01 10:07:36 -0500

    Logos in footer updated.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,39 @@
 import React from "react";
+import LinkedInPNG from '../components/pages/img/logos/linkedin.png'
+import GitHubPNG from '../components/pages/img/logos/github.png'
+import GmailPNG from '../components/pages/img/logos/gmail.png'
+import StackPNG from '../components/pages/img/logos/overflowing.png'
 
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <footer className="">
-            <div className="col-5 col-5-md col-10-sm contact-container-left">
-                <div><strong>email</strong></div>
-                <div><a href="mailto:giannifontanot@gmail.com">giannifontanot@gmail.com</a></div>
-                <div>&nbsp;</div>
-                <div><strong>Stackoverflow</strong></div>
-                <div><a href="https://stackoverflow.com/users/15772854/gianni-fontanot">https://stackoverflow.com/users/15772854/</a></div>
-            </div>
-            <div className="col-5 col-5-md col-10-sm text-right contact-container-right">
-                <div><strong>Linkedin</strong></div>
-                <div><a
-                    href="https://www.linkedin.com/in/gianni-fontanot">linkedin.com/in/gianni-fontanot</a>
-                </div>
-                <div>&nbsp;</div>
-                <div><strong>GitHub</strong></div>
-                <div><a href="https://github.com/giannifontanot">github.com/giannifontanot</a></div>
-            </div>
+            <div className="footer-logo-container">
 
+                <div>
+                    <div className="logo-container">
+
+                        <div><a href="mailto:giannifontanot@gmail.com"><img className="img-thumbnail logo-img" alt=""
+                                                                            src={GmailPNG}/></a></div>
+
+                        <div><a href="https://stackoverflow.com/users/15772854/gianni-fontanot"><img
+                            className="img-thumbnail logo-img" alt="" src={StackPNG}/></a></div>
+
+                        <div><a href="https://www.linkedin.com/in/gianni-fontanot"><img
+                            className="img-thumbnail logo-img" alt="" src={LinkedInPNG}/></a></div>
+
+                        <div><a href="https://github.com/giannifontanot"><img className="img-thumbnail logo-img" alt=""
+                                                                              src={GitHubPNG}/></a></div>
+                    </div>
+                </div>
+
+
+                <div>
+                    <span className="copyright-message">Icons made by <a href="https://www.freepik.com"
+                                                                         title="Freepik">Freepik</a> from <a
+                        href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></span>
+                </div>
+            </div>
         </footer>
     );
 }

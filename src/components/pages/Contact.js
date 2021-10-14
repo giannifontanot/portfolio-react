0d2ba35ed0e2973360992cd78ad719e7d99e1703
giannifontanot <giannifontanot@gmail.com>
2021-10-14 12:12:00 -0500

    Updated resume and contact page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -19,18 +19,18 @@ export default function Contact() {
                                 <h4>Drop me an email. Old-fashioned phone calls work too!</h4>
 < br />< br />
                                 <div className="form-email">
-                                <form className="pure-form pure-form-aligned">
-                                    <fieldset>
-                                        <NameInput InputName="name" id="aligned-name" placeholder="Please enter your name" />
-                                        <EmailInput InputName="email" id="aligned-email" placeholder="Please enter your email" />
-                                        <NameTextarea InputName="message" id="aligned-message" placeholder="Enter something here..." />
+                                {/*<form className="pure-form pure-form-aligned">*/}
+                                {/*    <fieldset>*/}
+                                {/*        <NameInput InputName="name" id="aligned-name" placeholder="Please enter your name" />*/}
+                                {/*        <EmailInput InputName="email" id="aligned-email" placeholder="Please enter your email" />*/}
+                                {/*        <NameTextarea InputName="message" id="aligned-message" placeholder="Enter something here..." />*/}
 
-                                        <div className="pure-controls">
-                                            <button type="submit" className="pure-button pure-button-primary">Submit
-                                            </button>
-                                        </div>
-                                    </fieldset>
-                                </form>
+                                {/*        <div className="pure-controls">*/}
+                                {/*            <button type="submit" className="pure-button pure-button-primary">Submit*/}
+                                {/*            </button>*/}
+                                {/*        </div>*/}
+                                {/*    </fieldset>*/}
+                                {/*</form>*/}
                                 </div>
                             </div>
                         </div>

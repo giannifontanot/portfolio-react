a87fdc9abe0318a582bc7f9eccc2a8d03353f8fe
Gianni Fontanot <giannifontanot@gmail.com>
2022-04-25 21:47:18 -0500

    New Versioln

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'purecss/build/pure.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); 
 
 
